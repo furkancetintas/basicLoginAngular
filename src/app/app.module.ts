65606a770c6e9898e6cd47fa7b264c9d5b4851c8
Furkan Çetintaş <furkancetintas01@gmail.com>
2022-06-28 22:37:34 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ProductAddComponent } from './modules/product-add/product-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
